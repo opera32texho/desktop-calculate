3d2455024200e0592697e56d907c0ef31966a4a0
Vitalii <Vitalii@bearanalytics.com>
2023-05-29 15:17:25 +0300

    prefent input

--- src/app/calculate-page/calculate-page.component.ts
+++ src/app/calculate-page/calculate-page.component.ts
@@ -8,23 +8,32 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CalculatePageComponent implements OnInit {
   calculatorForm: FormGroup;
+  previousValue: number;
   currencies: string[] = [
     'TUSD (Test US Dollar)',
     'TEUR (Test Euro)',
     'TCNY (Test Chinese Yuan)',
     'TINR (Test Indian Rupee)',
     'TBRL (Test Brazilian Real)',
-    'TIDR (Test Indonesian Rupiah)'
+    'TIDR (Test Indonesian Rupiah)',
   ];
   periods: number[] = [1, 3, 6, 12, 24];
   aprs: { [key: string]: number } = {
     'TUSD (Test US Dollar)': 0.12,
-    'TEUR (Test Euro)':0.13,
-    'TCNY (Test Chinese Yuan)':0.2,
+    'TEUR (Test Euro)': 0.13,
+    'TCNY (Test Chinese Yuan)': 0.2,
     'TINR (Test Indian Rupee)': 0.33,
     'TBRL (Test Brazilian Real)': 0.21,
     'TIDR (Test Indonesian Rupiah)': 0.8,
   };
+  persent: string[] = [
+    'APR 12%',
+    'APR 13%',
+    'APR 20%',
+    'APR 33%',
+    'APR 21%',
+    'APR 80%',
+  ];
 
   constructor(private fb: FormBuilder) {}
 
@@ -38,8 +47,10 @@ export class CalculatePageComponent implements OnInit {
       period: [1, Validators.required],
     });
 
-    this.calculatorForm.valueChanges.subscribe(() => {
+    this.calculatorForm.valueChanges.subscribe((res) => {
       this.calculateProfit();
+
+      console.log(res.amount)
     });
   }
 
@@ -57,6 +68,7 @@ export class CalculatePageComponent implements OnInit {
     }
   }
 
+
   calculateProfit(): number {
     const amount = this.calculatorForm.get('amount')?.value;
     const currency = this.calculatorForm.get('currency')?.value;
@@ -69,5 +81,16 @@ export class CalculatePageComponent implements OnInit {
     }
 
     return 0;
+  }
+  preventInput(event: KeyboardEvent) {
+    const currentValue = this.calculatorForm.controls.amount.value;
+    const enteredValue = Number(event.key);
+
+    if (currentValue >= 1000000 || currentValue * 10 + enteredValue > 1000000) {
+
+      this.calculatorForm.controls.amount?.setValue(this.previousValue);
+    }else {
+      this.previousValue = currentValue;
+    }
   }
 }
